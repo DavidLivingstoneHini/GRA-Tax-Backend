0f89a3205789ba73e9474beb3cfad6a1890d65d2
Livingstone David Kwame Hini <kwamelivingstone77@gmail.com>
2024-06-27 22:07:09 -0400

    feat: final updates

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express, { Express } from "express";
 import { salaryCalculatorRoutes } from "./routes/salaryCalculator.route";
+import cors from "cors";
+import * as dotenv from "dotenv";
 import swaggerUI from "swagger-ui-express";
 import swaggerDocument from "../swagger-output.json";
+
+dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3002;
 
+app.use(cors());
 app.use("/salary-calculator", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use(express.json());
